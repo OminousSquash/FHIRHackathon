b179a2a6d63f287b5595087830444f5779fd79fe
igreat <goraguy5@gmail.com>
2024-03-07 13:27:47 +0000

    allow load commit

--- src/utils.js
+++ src/utils.js
@@ -1,56 +1,71 @@
-// This file will try to fetch the templates from the firebase
-async function loadTemplates() {
-  // TODO: Load templates from the server
+//   // placeholder data for testing
+//   return [
+//     {
+//       id: "1",
+//       name: "covid form",
+//       fields: [
+//         {
+//           name: "blood-pressure",
+//           unit: "mmHg",
+//           description: "Enter patient blood pressure",
+//           type: "numeric",
+//           expected: 128,
+//         },
+//         {
+//           name: "temperature",
+//           unit: "degrees Celsius",
+//           description: "Enter patient temperature",
+//           type: "numeric",
+//           expected: 37,
+//         },
+//         {
+//           name: "urine-color",
+//           description: "Enter patient urine color",
+//           type: "textual",
+//           expected: "light yellow",
+//         },
+//       ],
+//     },
+//     {
+//       id: "2",
+//       name: "vitals form",
+//       fields: [
+//         {
+//           name: "blood-pressure",
+//           unit: "mmHg",
+//           description: "Enter patient blood pressure",
+//           type: "numeric",
+//           expected: 128,
+//         },
+//         {
+//           name: "temperature",
+//           unit: "degrees Celsius",
+//           description: "Enter patient temperature",
+//           type: "numeric",
+//           expected: 37,
+//         },
+//       ],
+//     },
+//   ];
+// }
 
-  // placeholder data for testing
-  return [
-    {
-      id: "1",
-      name: "covid form",
-      fields: [
-        {
-          name: "blood-pressure",
-          unit: "mmHg",
-          description: "Enter patient blood pressure",
-          type: "numeric",
-          expected: 128,
-        },
-        {
-          name: "temperature",
-          unit: "degrees Celsius",
-          description: "Enter patient temperature",
-          type: "numeric",
-          expected: 37,
-        },
-        {
-          name: "urine-color",
-          description: "Enter patient urine color",
-          type: "textual",
-          expected: "light yellow",
-        },
-      ],
-    },
-    {
-      id: "2",
-      name: "vitals form",
-      fields: [
-        {
-          name: "blood-pressure",
-          unit: "mmHg",
-          description: "Enter patient blood pressure",
-          type: "numeric",
-          expected: 128,
-        },
-        {
-          name: "temperature",
-          unit: "degrees Celsius",
-          description: "Enter patient temperature",
-          type: "numeric",
-          expected: 37,
-        },
-      ],
-    },
-  ];
+import { collection, getDocs } from "firebase/firestore";
+import db from "./pages/firebase";
+
+async function loadTemplates() {
+  console.log("Loading templates...");
+  const templatesCollectionRef = collection(db, "formTemplates");
+  console.log("Templates collection ref");
+  const querySnapshot = await getDocs(templatesCollectionRef);
+  console.log("Query snapshot");
+
+  const templates = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  console.log("Templates brooo", templates);
+  return templates;
 }
 
 export { loadTemplates };
