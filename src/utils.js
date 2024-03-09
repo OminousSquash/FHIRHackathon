76a2fbdddc4d9a82509ab4d3546fbe6afd3c4156
OminousSquash <zcabvsr@ucl.ac.uk>
2024-03-09 23:48:02 +0000

    update graphs issue

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,8 @@ import { collection, getDocs } from "firebase/firestore";
 import db from "./firebase";
 
 async function loadTemplates() {
-  console.log("Loading templates...");
   const templatesCollectionRef = collection(db, "formTemplates");
-  console.log("Templates collection ref");
   const querySnapshot = await getDocs(templatesCollectionRef);
-  console.log("Query snapshot");
-
   const templates = querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
